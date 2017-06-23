1d5530802948694ff8cc5b997823107ed0679af5
Jesse <jesse.li18@ycdsbk12.ca>
2017-06-22 23:55:34 -0400

    finished adding title

--- app/src/main/java/com/strobertchs/ics4u_finalproject_wukongshowdown/GameActivity.java
+++ app/src/main/java/com/strobertchs/ics4u_finalproject_wukongshowdown/GameActivity.java
@@ -1,22 +1,7 @@
 package com.strobertchs.ics4u_finalproject_wukongshowdown;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.PointF;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
-import android.widget.ImageView;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -25,4 +10,6 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
     }
+
+
 }
