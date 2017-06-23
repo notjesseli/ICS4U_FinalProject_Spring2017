ed5fe77dc51818ee34bf8ef5ad9464ce2841d789
Jesse <jesse.li18@ycdsbk12.ca>
2017-06-23 14:23:30 -0400

    updated

--- app/src/main/java/com/strobertchs/ics4u_finalproject_wukongshowdown/MainActivity.java
+++ app/src/main/java/com/strobertchs/ics4u_finalproject_wukongshowdown/MainActivity.java
@@ -3,8 +3,10 @@ package com.strobertchs.ics4u_finalproject_wukongshowdown;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -21,5 +23,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent i;
         i = new Intent(this, GameActivity.class);
         startActivity(i);
+    }
+
+    public void ruleClick(View v){
+        //Outputs a message when clicking the rule button.
+        Toast.makeText(this, "This is a reactive game similar to Whack-a-mole!",
+                Toast.LENGTH_SHORT).show();
+        Log.i("info","The user clicked the help button.");
+        Intent i;
+    }
+
+    public void quitClick(View v) {
+        finish();
+        System.exit(0);
     }
 }
